d58506f3b7a3cf2cb7d79cef1671275562849259
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-17 18:52:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(" and ")
       elsif array.length == 3
-        array.join(", ") " and " array.last
+        array.join(", "  " and " array.last)
       else array.length > 3
         array.to_sentence
   end
