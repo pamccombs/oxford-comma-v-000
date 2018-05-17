51ea4c381cea2c15b6932d855885a42bd455462f
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-17 18:57:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif array.length == 2
       array.join(" and ")
       elsif array.length == 3
-        "#{array.join(', ')} and #{array.last}"
+        "#{array[0...-1].join(', ')} and #{array.last}"
       else array.length > 3
         array.to_sentence
   end
