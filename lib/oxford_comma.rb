e1d24f836d9ca82984abbd4ecaf9f6755cd0ef30
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-17 18:42:32 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,10 @@ def oxford_comma(array)
   if array.length == 1
     array.join
     elsif array.length == 2
-      array.to_sentence(two_words_connector: ' and ')
+      array.to_sentence
       elsif array.length == 3
         array.to_sentence
-        
+      else array.length > 3
+        array.to_sentence
   end
 end
