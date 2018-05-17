1e201eba88eb02195b26824602246e56ef2ee15e
Patrick McCombs <pa.mccombs1@gmail.com>
2018-05-17 18:44:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
     array.join
     elsif array.length == 2
-      array.to_sentence(1)
+      array.to_set
       elsif array.length == 3
         array.to_sentence
       else array.length > 3
